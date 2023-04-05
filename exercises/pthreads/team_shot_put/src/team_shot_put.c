d9cb2d01b1dd748b94461202668ac5ab3516417b
Gabriel Alba <gabriel.alba@ucr.ac.cr>
2023-04-04 23:19:58 -0600

    Changing logic for generating threads

--- exercises/pthreads/team_shot_put/src/team_shot_put.c
+++ exercises/pthreads/team_shot_put/src/team_shot_put.c
@@ -7,46 +7,56 @@
 #include <time.h>
 #include <inttypes.h>
 
-uint64_t* team_throws(void* data);
+void* team_throws(void* data);
 
 typedef struct team_shot_put {
     uint64_t numOfPlayers;
     uint64_t numofThrows;
-} team_shot_put;
+} team_shot_put_type;
 
 
 typedef struct privateData {
     uint64_t teamNumber;
     uint64_t* teamScores;
-    team_shot_put* publicData;
+    team_shot_put_type* publicData;
 
-} privateData;
+} privateData_type;
 
 
 // procedure main:
 int main(void) {
+
+    srand(time(NULL));
     uint64_t numPlayersByTeam = 8;
     uint64_t numOfShotsByTeam = 3;
-    uint64_t numOfTeams = 2;
-    team_shot_put publicData;
-    privateData* teamData = calloc(numOfTeams, sizeof(privateData));
-    teamData->publicData->numOfPlayers = numPlayersByTeam;
-    teamData->publicData->numofThrows = numOfShotsByTeam;
-    pthread_t* thread = calloc(2, sizeof(pthread_t*));
-    for (uint64_t team = 0; team < numOfTeams; team++) {
-        teamData->teamNumber = team + 1;
-        int error = pthread_create(&thread[team], NULL, team_throws, &teamData[team]);
-    }
+    privateData_type* teamOneData = (privateData_type*) calloc(1, sizeof(privateData_type));
+    privateData_type* teamTwoData = (privateData_type*) calloc(1, sizeof(privateData_type));
+    team_shot_put_type* publicData = (team_shot_put_type*) calloc(1, sizeof(team_shot_put_type));
+    pthread_t threadOne;
+    pthread_t threadTwo;
+    publicData->numOfPlayers = numPlayersByTeam;
+    publicData->numofThrows = numOfShotsByTeam;
+    teamOneData->teamNumber = 1;
+    teamTwoData->teamNumber = 2;
 
-    free(thread);
-    free(teamData);
+    int error1 = pthread_create(&threadOne, NULL, team_throws, &teamOneData);
+    int error2 = pthread_create(&threadTwo, NULL, team_throws, &teamTwoData);
+    printf("Thread created\n");
+
+    pthread_join(threadOne, NULL);
+    pthread_join(threadTwo, NULL);
+
+    free(teamOneData);
+    free(teamTwoData);
     return EXIT_SUCCESS;
 }
 // end procedure
 
-uint64_t* team_throws(void* teamData) {
-    privateData* privateData = teamData;
-    team_shot_put* publicData = privateData->publicData;
+void* team_throws(void* teamData) {
+    privateData_type* privateData = teamData;
+    team_shot_put_type* publicData = privateData->publicData;
+    printf(publicData->numOfPlayers);
+    printf(publicData->numofThrows);
     uint64_t* teamScores = calloc(publicData->numOfPlayers,sizeof(uint64_t));
     for (uint64_t player = 0; player < publicData->numOfPlayers;) {
         uint64_t playerScore = 0;
@@ -56,9 +66,15 @@ uint64_t* team_throws(void* teamData) {
                 playerScore = newPlayerScore;
             }
         }
+        printf(player);
+        printf(", ");
+        printf(playerScore);
+        printf("\n");
         teamScores[player] = playerScore;
     }
 
-    return teamScores;
+    privateData->teamScores = teamScores;
+    free(teamScores);
+    return privateData->teamScores;
 }
 
