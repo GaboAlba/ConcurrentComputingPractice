2507315924508af21cadc1eb5b69f6a4dd7ec694
Gabriel Alba <gabriel.alba@ucr.ac.cr>
2023-04-07 19:20:22 -0600

    Adding birthday pinata

---
+++ exercises/pthreads/birthday_pinata/src/birthday_pinata.c
@@ -0,0 +1,23 @@
+// Copyright [2023] Gabriel Alba Romero <gabriel.alba@ucr.ac.cr>
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <inttypes.h>
+
+int hitPinata(void* data);
+
+// procedure main:
+int main(void) {
+
+    int hitsToBreak = 10;
+    int threadsInvited = 4;
+
+    return EXIT_SUCCESS;
+}
+// end procedure
+
+
+
