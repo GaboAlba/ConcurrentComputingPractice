6db480983d23fb26fd090195aadb31cc82679039
Gabriel Alba <gabriel.alba@ucr.ac.cr>
2023-04-08 12:32:33 -0600

    Creating structure for zippass_serial

---
+++ tareas/zippass_serial/src/zippass_serial.c
@@ -0,0 +1,25 @@
+// Copyright [2023] Gabriel Alba Romero <gabriel.alba@ucr.ac.cr>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <inttypes.h>
+#include <zip.h>
+
+
+int validateInput() {
+    return 0;
+}
+
+int validatePrerequisites() {
+    return 0;
+}
+
+char* descipherPassword(char* file_path) {
+    return "PASS";
+}
+
+int main(void) {
+    return 0;
+}
---
+++ tareas/zippass_serial/test/tests.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <inttypes.h>
+#include <zip.h>
+
+int openWithNoPassword_Test() {
+    return 0;
+}
+
+int returnsCorrectPassword() {
+    return 0;
+}
+
+int failWithNotASCIICharacters_Test() {
+    return 0;
+}
+
+int failIfFilesDontExist_Test() {
+    return 0;
+}
