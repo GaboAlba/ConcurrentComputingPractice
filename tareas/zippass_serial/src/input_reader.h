95ed9ecb4724315bd7f94fe6c11540276fdce2e6
Gabriel Alba <gabriel.alba@ucr.ac.cr>
2023-04-08 22:28:32 -0600

    Create header for operations on std_in

---
+++ tareas/zippass_serial/src/input_reader.h
@@ -0,0 +1,48 @@
+// Taken from https://stackoverflow.com/questions/17983005/c-how-to-read-a-string-line-by-line
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <zip.h>
+#include <string.h>
+
+int countChar(char* str, char c) {
+  char* nextChar = strchr(str, c);
+  int count = 0;
+
+  while (nextChar) {
+    count++;
+    nextChar = strchr(nextChar + 1, c);
+  }
+
+  return count;
+}
+
+char** lineator(char* origin) {
+  char* str = (char*) malloc(strlen(origin) + 1);
+  strcpy(str, origin);
+
+  int count = countChar(origin, '\n');
+  char** lines = (char**) malloc(sizeof(char *) * count);
+
+  char* nextLine = strchr(str, '\n');
+  char* currentLine = str;
+
+  int i = 0;
+
+  while (nextLine) {
+    *nextLine = '\0';
+
+    lines[i] = malloc(strlen(currentLine) + 1);
+    strcpy(lines[i], currentLine);
+
+    currentLine = nextLine + 1;
+    nextLine = strchr(currentLine, '\n');
+
+    i++;
+  }
+
+  free(str);
+  return lines;
+}
