f17c1f1b76153fafa9dc588a581ccc7613020910
gabriel.alba <gabriel.alba@ucr.ac.cr>
2023-04-11 01:43:37 -0600

    Modularizing main routine

--- tareas/zippass_serial/src/zippass_serial.c
+++ tareas/zippass_serial/src/zippass_serial.c
@@ -9,9 +9,11 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "input_reader.h"
-
+#include "password_logic.h"
+#include "zip_operations_test.h"
 
 int validateASCIIInput(char input[]) {
     uint64_t inputSize = strlen(input);
@@ -32,90 +34,6 @@ int validatePrerequisites() {
     return error;
 }
 
-bool decrypt_zip(char* file_path, char* password) {
-    int error;
-    zip_t* file = zip_open(file_path, ZIP_RDONLY, &error);
-    if (file == NULL) {
-        fprintf(stderr, "ERROR: Failed to open file %s\n", zip_strerror(file));
-        return false;
-    }
-}
-
-char* generateNextPassword(uint64_t* testCounters, char* alphabet) {
-
-}
-
-char* descipherPassword_Serial(char* file_path, char* alphabet, uint64_t maxPwdLength) {
-    char* password = calloc(maxPwdLength, sizeof(char));
-    uint64_t* testCounters = calloc(maxPwdLength, sizeof(uint64_t));
-    uint64_t sizeOfAlphabet = strlen(alphabet);
-    bool exitCondition = false;
-    bool success = false;
-    
-    // init password array
-    for (uint64_t character = 0; character < maxPwdLength; character++) {
-        if (character == 0) {
-            password[character] = alphabet[character];
-            testCounters[character] = 0;
-        }
-        password[character] = NULL;
-        testCounters[character] = NULL;
-    }
-
-    // Brute force descipher password
-    uint64_t pwdLength = 1;
-    while (!exitCondition) {
-        // Loop around the testCounters and look for the one that is not the maxCounter
-        for (uint64_t cell = 0; cell < maxPwdLength; cell++) {
-            if (testCounters[cell] == sizeOfAlphabet - 1) {
-                if(testCounters[cell + 1] == NULL) {
-                    pwdLength++;
-                    testCounters[cell + 1] = 0;
-                    break;
-                }
-            } else {
-                testCounters[cell]++;
-                // Clean all previous cells to first character when changing that one
-                for (uint64_t clears = cell - 1; clears >= 0; clears--) {
-                    testCounters[clears] = 0;
-                }
-                break;
-            }
-        }
-
-        // Set password to the current combination
-        for (uint64_t counter = 0; counter < pwdLength; counter++) {
-            password[counter] = alphabet[testCounters[counter]];
-        }
-
-        // Check if all passwords have been tried
-        for (uint64_t counter = 0; counter < maxPwdLength; counter++) {
-            if (testCounters[counter] == sizeOfAlphabet - 1) {
-                exitCondition = true;
-            } else {
-                exitCondition = false;
-            }
-        }
-
-        // Tries generated password if not all passwords have been tested
-        if (!exitCondition) {
-            exitCondition = decrypt_zip(file_path, password);
-        } else {
-            return "";
-        }
-        
-
-        
-        
-    }
-    
-    return password;
-}
-
-/*@brief To be implemented */
-char* descipherPassword_Parallel(char* file_path) {
-    return "PASS";
-}
 
 int main(void) {
     int64_t maxCharacters = 999;
@@ -135,24 +53,30 @@ int main(void) {
     error = validateASCIIInput(input);
     uint64_t lineCount = countChar(input, '\n');
     uint64_t fileCount = lineCount - 3;
-    printf("LC: %" PRIu64 "\n", lineCount);
     char** lines = lineator(input);
     char* alphabet = lines[0];
+    alphabet = strcat(alphabet,"\0");
     uint64_t maxLength = strtoull(lines[1], NULL, 10);
     char** files = (char**) calloc(lineCount-3,sizeof(char*));
     for (uint64_t line = 3; line < lineCount; line++) {
         files[line - 3] = lines[line];
-        printf("Registered: %s\n", files[line-3]);
     }
-    printf("ABC: %s\n",alphabet);
-    printf("Length: %" PRIu64 "\n" , maxLength);
-    printf("FirstFile: %s\n", files[0]);
-    printf("SecondFile: %s\n", files[1]);
-
+    // Decipher passwords
+    struct timespec start_time, finish_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    char** passwords = calloc(fileCount, sizeof(char*));
     for (uint64_t file = 0; file < fileCount; file++) {
-        descipherPassword_Serial(files[file], alphabet, maxLength);
+        passwords[file] = descipherPassword_Serial(files[file], alphabet, maxLength);
     }
+    clock_gettime(CLOCK_MONOTONIC, &finish_time);
+    double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
+      (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
 
+    for (uint64_t password = 0; password < fileCount; password++) {
+        printf("%s %s\n", files[password], passwords[password]);
+    }
+    printf("Time: %.9lfs\n", elapsed_time);
     free(files);
+    free(passwords);
     return error;
 }
