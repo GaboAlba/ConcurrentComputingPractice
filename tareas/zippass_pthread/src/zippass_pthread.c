5f1bc87a22b8828f83045cd03df3e263c5b9ec08
gabriel.alba <gabriel.alba@ucr.ac.cr>
2023-04-25 23:42:26 -0600

    Adding Debug constant for testing purposes

--- tareas/zippass_pthread/src/zippass_pthread.c
+++ tareas/zippass_pthread/src/zippass_pthread.c
@@ -2,6 +2,8 @@
 ///  @file
 ///  @brief Main source program
 
+#define DEBUGCONST 1
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,21 +19,8 @@
 #include "input_reader.h"
 #include "password_logic.h"
 #include "zip_operations_test.h"
-
-/// @brief Struct that defines the data that can be accessed by any file and/or
-///        thread
-typedef struct publicData {
-    char* alphabet;
-    uint8_t maxLength;
-} publicData_t;
-
-/// @brief Struct that defines the data that is property of the file and/or
-///        thread and may not be shared among them
-typedef struct privateData {
-    char* filePath;
-    char* password;
-    publicData_t* publicData;
-} privateData_t;
+#include "exec_time_test.h"
+#include "data_structures.h"
 
 //**********************************************************************************************************************
 ///    @brief Asks the user for the input and validates it. After this it calls
@@ -42,71 +31,80 @@ typedef struct privateData {
 ///        @retval int for any error
 //**********************************************************************************************************************
 int main(void) {
-  const uint16_t kMaxCharacters = 999;
-  char input[kMaxCharacters];
-  printf("Please enter required information (use ; when finished): \n");
-  int8_t error = scanf("%[^;]s", &input);
-  if (error > 0) {
+  if (!DEBUGCONST) {
+    const uint16_t kMaxCharacters = 999;
+    char input[kMaxCharacters];
+    printf("Please enter required information (use ; when finished): \n");
+    int8_t error = scanf("%[^;]s", &input);
+    if (error > 0) {
+    } else {
+      fprintf(stderr, "ERROR: Could not read input\n");
+      return error;
+    }
+    error = validateASCIIInput(input);
+    if (error) {
+      return error;
+    }
+    uint8_t lineCount = countChar(input, '\n');
+    uint8_t fileCount = lineCount - 3;
+    char** lines = lineator(input);
+    error = validateInputFormat(lines, lineCount);
+    if (error) {
+      return error;
+    }
+    publicData_t* publicData = calloc(1, sizeof(publicData_t));
+    publicData->alphabet = calloc(128, sizeof(char));
+    publicData->alphabet = lines[0];
+    publicData->alphabet = strcat(publicData->alphabet, "\0");
+    publicData->maxLength = strtoull(lines[1], NULL, 10);
+    privateData_t* files = (privateData_t*) calloc(lineCount-3,
+                                                  sizeof(privateData_t));
+
+    // Assign files to a single data structure
+    for (uint8_t line = 3; line < lineCount; line++) {
+      files[line - 3].filePath = lines[line];
+    }
+
+    // Print whitespace for better visualization
+    for (int8_t spaces = 0; spaces < 5; spaces++) {
+      printf("\n");
+    }
+    printf("---Decrypting your password(s)---\n");
+
+    // Start Timer
+    struct timespec start_time, finish_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    // Decipher passwords
+    for (uint8_t file = 0; file < fileCount; file++) {
+      files[file].password = calloc(publicData->maxLength, sizeof(char));
+      files[file].publicData = publicData;
+      files[file].password = descipherPassword_Serial(files[file].filePath,
+                                                      publicData->alphabet,
+                                                      publicData->maxLength,
+                                                      files[file].password);
+      printf("%s %s\n", files[file].filePath, files[file].password);
+    }
+
+    // Stop taking time
+    clock_gettime(CLOCK_MONOTONIC, &finish_time);
+    double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
+      (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+
+    printf("---Password(s) decrypted---\n");
+    printf("Time: %.9lfs\n", elapsed_time);
+
+    // Free up memory
+    free(publicData);
+    free(lines);
+    free(files);
+    return error;
   } else {
-    fprintf(stderr, "ERROR: Could not read input\n");
-    return error;
+    // ENTER HERE THE TESTS YOU WANT TO RUN 
+    passwordGenTimer();
+    decryptTesterTimerWrong();
+    decryptTesterTimerRight();
+    canFileBeOpenedMultipleTimes();
+
   }
-  error = validateASCIIInput(input);
-  if (error) {
-    return error;
-  }
-  uint8_t lineCount = countChar(input, '\n');
-  uint8_t fileCount = lineCount - 3;
-  char** lines = lineator(input);
-  error = validateInputFormat(lines, lineCount);
-  if (error) {
-    return error;
-  }
-  publicData_t* publicData = calloc(1, sizeof(publicData_t));
-  publicData->alphabet = calloc(128, sizeof(char));
-  publicData->alphabet = lines[0];
-  publicData->alphabet = strcat(publicData->alphabet, "\0");
-  publicData->maxLength = strtoull(lines[1], NULL, 10);
-  privateData_t* files = (privateData_t*) calloc(lineCount-3,
-                                                 sizeof(privateData_t));
-
-  // Assign files to a single data structure
-  for (uint8_t line = 3; line < lineCount; line++) {
-    files[line - 3].filePath = lines[line];
-  }
-
-  // Print whitespace for better visualization
-  for (int8_t spaces = 0; spaces < 5; spaces++) {
-    printf("\n");
-  }
-  printf("---Decrypting your password(s)---\n");
-
-  // Start Timer
-  struct timespec start_time, finish_time;
-  clock_gettime(CLOCK_MONOTONIC, &start_time);
-
-  // Decipher passwords
-  for (uint8_t file = 0; file < fileCount; file++) {
-    files[file].password = calloc(publicData->maxLength, sizeof(char));
-    files[file].publicData = publicData;
-    files[file].password = descipherPassword_Serial(files[file].filePath,
-                                                    publicData->alphabet,
-                                                    publicData->maxLength,
-                                                    files[file].password);
-    printf("%s %s\n", files[file].filePath, files[file].password);
-  }
-
-  // Stop taking time
-  clock_gettime(CLOCK_MONOTONIC, &finish_time);
-  double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
-    (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
-
-  printf("---Password(s) decrypted---\n");
-  printf("Time: %.9lfs\n", elapsed_time);
-
-  // Free up memory
-  free(publicData);
-  free(lines);
-  free(files);
-  return error;
 }
