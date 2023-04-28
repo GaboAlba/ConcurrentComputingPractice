3af17341bb27cebc97d1a264b402042d10175d0b
gabriel.alba <gabriel.alba@ucr.ac.cr>
2023-04-28 09:15:03 -0600

    Created the queue manager

--- tareas/zippass_pthread/src/queue_manager.c
+++ tareas/zippass_pthread/src/queue_manager.c
@@ -8,15 +8,38 @@
 #include <stdbool.h>
 #include <pthread.h>
 
-char** createQueue(uint64_t QueueSize){
-    char** Queue = calloc(QueueSize, sizeof(char*));
-    return Queue;
+#include "data_structures.h"
+
+void createQueue(QueueData_t QueueData){
+    QueueData.Queue = calloc(QueueData.QueueMaxSize, sizeof(char*));
 }
 
-char* dequeue(char** Queue) {
-
+char* dequeue(QueueData_t QueueData) {
+    char* password = QueueData.Queue[QueueData.front++];
+	
+    if(QueueData.front == QueueData.QueueMaxSize) {
+        QueueData.front = 0;
+    }
+	
+    QueueData.QueueCurrentSize--;
+    return password;
 }
 
-void enqueue(char** Queue, char* password) {
+void enqueue(QueueData_t QueueData, char* password) {
+    if (!isFull(QueueData)) {
+        if(QueueData.rear == QueueData.QueueMaxSize - 1) {
+          QueueData.rear = -1;            
+        }       
 
+      QueueData.Queue[++QueueData.rear] = password;
+      QueueData.QueueCurrentSize++;
+    }
+}
+
+bool isFull(QueueData_t QueueData) {
+    return QueueData.QueueCurrentSize == QueueData.QueueMaxSize;
+}
+
+bool isEmpty(QueueData_t QueueData) {
+    return QueueData.QueueCurrentSize == 0;
 }
