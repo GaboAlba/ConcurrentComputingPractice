d1a064a8d73f4ccfade0ed8b2399fc6f6b151dda
gabriel.alba <gabriel.alba@ucr.ac.cr>
2023-05-23 21:30:34 -0600

    std_thread_cpp exercise done

--- exercises/pthreads/std_thread_cpp/src/std_thread_cpp.cpp
+++ exercises/pthreads/std_thread_cpp/src/std_thread_cpp.cpp
@@ -1,120 +1,117 @@
-// Copyright 2021 Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr> CC-BY 4.0
-
 #include <assert.h>
-#include <inttypes.h>
-#include <pthread.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
 #include <thread>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
+#include <vector>
+#include <mutex>
 
 // thread_shared_data_t
-typedef struct shared_data {
+struct shared_data {
   uint64_t thread_count;
-} shared_data_t;
+};
 
 // thread_private_data_t
-typedef struct private_data {
+struct private_data {
   uint64_t thread_number;  // rank
-  shared_data_t* shared_data;
-} private_data_t;
+  std::shared_ptr<shared_data> Shared_data;
+};
 
 /**
  * @brief ...
  */
-void* greet(void* data);
-int create_threads(shared_data_t* shared_data);
+void greet(void* data);
+int create_threads(std::shared_ptr<shared_data> shared_data);
 
 // procedure main(argc, argv[])
 int main(int argc, char* argv[]) {
   int error = EXIT_SUCCESS;
   // create thread_count as result of converting argv[1] to integer
   // thread_count := integer(argv[1])
-  uint64_t possibleThreads = std::thread()
-  uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
+  uint64_t thread_count = std::thread::hardware_concurrency();
   if (argc == 2) {
-    if (sscanf(argv[1], "%" SCNu64, &thread_count) == 1) {
-    } else {
-      fprintf(stderr, "Error: invalid thread count\n");
+    std::string thread_count_str(argv[1]);
+    uint64_t thread_count = std::stoi(thread_count_str);
+    if (thread_count == 0) {
+      std::cerr << "Error: invalid thread count\n";
       return 11;
     }
   }
 
-  shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
-  if (shared_data) {
-    shared_data->thread_count = thread_count;
+  // create shared_data object
+  shared_data Shared_data;
+  Shared_data.thread_count = thread_count;
 
-    struct timespec start_time, finish_time;
-    clock_gettime(CLOCK_MONOTONIC, &start_time);
+  std::shared_ptr<shared_data> shared_data_ptr = std::make_shared<shared_data>();
+  if (shared_data_ptr) {
+    shared_data_ptr->thread_count = thread_count;
 
-    error = create_threads(shared_data);
+    std::chrono::high_resolution_clock::time_point start_time =
+        std::chrono::high_resolution_clock::now();
 
-    clock_gettime(CLOCK_MONOTONIC, &finish_time);
-    double elapsed_time = finish_time.tv_sec - start_time.tv_sec +
-      (finish_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+    error = create_threads(shared_data_ptr);
 
-    printf("Execution time: %.9lfs\n", elapsed_time);
+    std::chrono::high_resolution_clock::time_point finish_time =
+        std::chrono::high_resolution_clock::now();
+    double elapsed_time =
+        std::chrono::duration_cast<std::chrono::duration<double>>(
+            finish_time - start_time)
+            .count();
 
-    free(shared_data);
+    std::cout << "Execution time: " << elapsed_time << "\n";
   } else {
-    fprintf(stderr, "Error: could not allocate shared data\n");
+    std::cerr << "Error: could not allocate shared data\n";
     return 12;
   }
   return error;
 }  // end procedure
 
-int create_threads(shared_data_t* shared_data) {
+std::mutex Mutex;
+
+int create_threads(std::shared_ptr<shared_data> shared_data) {
   int error = EXIT_SUCCESS;
-  // for thread_number := 0 to thread_count do
-  pthread_t* threads = (pthread_t*)
-    malloc(shared_data->thread_count * sizeof(pthread_t));
-  private_data_t* private_data = (private_data_t*)
-    calloc(shared_data->thread_count, sizeof(private_data_t));
-  if (threads && private_data) {
-    for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
-        ; ++thread_number) {
-      private_data[thread_number].thread_number = thread_number;
-      private_data[thread_number].shared_data = shared_data;
-      // create_thread(greet, thread_number)
-      error = pthread_create(&threads[thread_number], /*attr*/ NULL, greet
-        , /*arg*/ &private_data[thread_number]);
-      if (error == EXIT_SUCCESS) {
-      } else {
-        fprintf(stderr, "Error: could not create secondary thread\n");
-        error = 21;
-        break;
-      }
-    }
+  // for thread_number := 0 to thread_count - 1 do
+  std::vector<std::thread> threads;
+for (uint64_t thread_number = 0; thread_number < shared_data->thread_count;
+     ++thread_number) {
+  // Create a private_data object and assign values
+  private_data* data = new private_data();
+  data->thread_number = thread_number;
+  data->Shared_data = shared_data;
 
-    // print "Hello from main thread"
-    printf("Hello from main thread\n");
+  // Create a thread and add it to the vector
+  threads.emplace_back(greet, data);
+}
 
-    for (uint64_t thread_number = 0; thread_number < shared_data->thread_count
-        ; ++thread_number) {
-      pthread_join(threads[thread_number], /*value_ptr*/ NULL);
-    }
+  // print "Hello from main thread"
+  std::cout << "Hello from main thread\n" << std::endl;;
 
-    free(private_data);
-    free(threads);
-  } else {
-    fprintf(stderr, "Error: could not allocate %" PRIu64 " threads\n"
-      , shared_data->thread_count);
-    error = 22;
+  for (auto& thread : threads) {
+    // join the thread
+    //threads.emplace_back(std::move(thread));
+    thread.join();
   }
 
   return error;
 }
 
 // procedure greet:
-void* greet(void* data) {
+void greet(void* data) {
   // assert(data);
-  private_data_t* private_data = (private_data_t*) data;
-  shared_data_t* shared_data = private_data->shared_data;
+  private_data* Private_data = static_cast<private_data*>(data);
+  std::shared_ptr<shared_data> shared_data = Private_data->Shared_data;
+
+  // try to lock the mutex
+  std::unique_lock<std::mutex> lock(Mutex);
 
   // print "Hello from secondary thread"
-  printf("Hello from secondary thread %" PRIu64 " of %" PRIu64 "\n"
-    , private_data->thread_number, shared_data->thread_count);
-  return NULL;
-}  // end procedure
+  std::cout << "Hello from secondary thread " << Private_data->thread_number
+            << " of " << shared_data->thread_count << "\n";
+
+
+  // access the shared_data object
+  // do nothing
+
+  // release the lock
+  lock.unlock();
+}
